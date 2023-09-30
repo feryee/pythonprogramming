db3a2c4af81dfd54cca865fb6731bf39e8d5c716
feryee <145667844+feryee@users.noreply.github.com>
2023-09-30 16:08:25 +0530

    Delete futureleapyr.py

--- futureleapyr.py
+++
@@ -1,8 +0,0 @@
-print(" start year")
-C=int(input("enter current year:"))
-print("Last year")
-L=int(input("enter last year:"))
-for year in range(C,L):
-    if(year%4==0)and(0!=year%100):
-        print(year)
-        
