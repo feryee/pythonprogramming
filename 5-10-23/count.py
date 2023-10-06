906ea539801175700c9415920c5e4b1869d34880
feryee <145667844+feryee@users.noreply.github.com>
2023-10-06 16:15:57 +0530

    Delete 5-10-23/count.py

--- 5-10-23/count.py
+++
@@ -1,8 +0,0 @@
-list=["ferin","shaana","amina","saraa"]
-count=0
-a=input("enter the letter to find:-")
-for i in list:
-    for x in i:
-        if x=='a':
-            count=count+1
-print("The occurrence of 'a' is :",count)
